fe8dfde04b065fe1363c030e8d519cb0973e9031
Cookie-Runa <112262822+Cookie-Runa@users.noreply.github.com>
2023-02-14 00:11:57 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -2,19 +2,19 @@ import requests
 import json
 
 def pin_to_ipfs(data):
-	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
+    assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
     #YOUR CODE HERE
     data_str = json.dumps(data)
+    project_id = '2LgtEEG00T4DgyqA1r0k1Q28nUh'
+    secret = '9b6f892c865160dfcfc002913a6838ef'
     response = requests.post('https://ipfs.infura.io:5001/api/v0/add',
-		id = '2LgtEEG00T4DgyqA1r0k1Q28nUh'
-		secret = '9b6f892c865160dfcfc002913a6838ef'
-                             files={'file' : data_str},auth=(id, secret))
+                             files={'file' : data_str},auth=(project_id, secret))
     return_value = response.text
     cid = json.loads(return_value)['Hash']
     return 
 
 def get_from_ipfs(cid,content_type="json"):
-		assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
+    assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
     #YOUR CODE HERE
     url = 'https://ipfs.infura.io:5001/api/v0/cat?arg=' + cid
     params = (('arg',cid),)
