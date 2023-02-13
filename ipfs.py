085107e0330cf2ed75c09e9956f9d5a6efd759cd
Cookie-Runa <112262822+Cookie-Runa@users.noreply.github.com>
2023-02-13 23:55:43 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -7,8 +7,8 @@ def pin_to_ipfs(data):
     json_data = json.dumps(data)
     files = {'file':json_data}
     url = 'https://gateway.pinata.cloud/ipfs/'
-    project_id = "ae3517feada4819223f6"
-    project_secret = '4c15fec94e9a620015a96dca3b3d525b3f49765629e949c9cb6e4ef9fdcfbee1'
+    project_id = "QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH"
+    project_secret = 'ae3517feada4819223f6'
     response = requests.post(url, files=files, auth=(project_id, project_secret))
     p = response.json()
     cid = p['Hash']
@@ -18,8 +18,8 @@ def pin_to_ipfs(data):
 def get_from_ipfs(cid,content_type="json"):
     assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
     params = (('arg', cid),)
-    project_id = "ae3517feada4819223f6"
-    project_secret = '4c15fec94e9a620015a96dca3b3d525b3f49765629e949c9cb6e4ef9fdcfbee1'
+    project_id = "QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH"
+    project_secret = 'ae3517feada4819223f6'
     response = requests.post('https://gateway.pinata.cloud/ipfs/', params=params, auth=(project_id, project_secret))
     data = json.loads(response.text)
     assert isinstance(data, dict), f"get_from_ipfs should return a dict"
